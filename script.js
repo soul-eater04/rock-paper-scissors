9f11ef813c0291c810c4d5659835a896ff1515b1
soul-eater04 <parthshresth04.k@gmail.com>
2023-12-08 02:11:31 +0530

    ADD GUI

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
 function getComputerChoice(){
-    const choices = ["rock","paper","scissors"];
+    const choices = ["rock","paper","scissor"];
     const randomNumber = Math.floor(Math.random()*3);
     return choices[randomNumber];
-}
-
-function play(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === computerSelection) {
-      return "It's a tie!";
-    } else if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-      return "You Win!";
-    } else {
-      return "You Lose!";
-    }
   }
   
-  function game(){
-    let playerSelection;
-    let computerSelection;
-    for(let i = 0 ; i < 5 ; i++){
-        playerSelection = prompt("Enter your choice:");
-        computerSelection = getComputerChoice();
-        let result = play(playerSelection,computerSelection);
-        console.log(computerSelection)
-        console.log(result);
+    function play(e,p,c) {
+      let computerSelection = getComputerChoice();
+      console.log(e.target.textContent.toLowerCase()+" "+computerSelection);
+      
+      // e.target.textContent = e.target.textContent.toLowerCase();
+      // p.textContent = e.target.textContent
+      // playerSelection = playerSelection.toLowerCase();
+    if (e.target.textContent.toLowerCase() === computerSelection) {
+      p.textContent = String(p.textContent);
+      c.textContent = String(c.textContent);
+    } else if (
+      (e.target.textContent.toLowerCase()=== "rock" && computerSelection === "scissor") ||
+      (e.target.textContent.toLowerCase() === "paper" && computerSelection === "rock") ||
+      (e.target.textContent.toLowerCase() === "scissor" && computerSelection === "paper")
+      ) {
+        p.textContent = String(Number(p.textContent)+1);
+      } else {
+        c.textContent = String(Number(c.textContent)+1);
+      }
     }
+    
+    function game(){
+    const body = document.querySelector('body');
+    const btn1 = document.createElement('button');
+    btn1.textContent = "ROCK";
+    const btn2 = document.createElement('button');
+    btn2.textContent = "PAPER";
+    const btn3 = document.createElement('button');
+    btn3.textContent = "SCISSOR";
+    body.appendChild(btn1);
+    body.appendChild(btn2);
+    body.appendChild(btn3);
+    const div = document.createElement('div');
+    const h1 = document.createElement('h1');
+    h1.textContent = "RESULT";
+    const p = document.createElement('p');
+    const c = document.createElement('p');
+    p.textContent = 0;
+    c.textContent = 0;
+    div.appendChild(h1);
+    div.appendChild(p);
+    div.appendChild(c);
+    body.appendChild(div);
+    btn1.addEventListener('click', (e) => play(e,p,c));
+    btn2.addEventListener('click', (e) => play(e,p,c));
+    btn3.addEventListener('click', (e) => play(e,p,c));
+
+    // let playerSelection;
+    // let computerSelection;
+    // playerSelection = prompt("Enter your choice:");
+    // computerSelection = getComputerChoice();
+    // let result = play(playerSelection,computerSelection);
+    // console.log(computerSelection)
+    // console.log(result);
   }
+
 game();
